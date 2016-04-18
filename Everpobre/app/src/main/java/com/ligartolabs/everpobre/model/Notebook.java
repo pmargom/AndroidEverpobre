a68aaa8133d2b09a3ffda1d349e077814c2a0932
Pedro Martín <pmargom@gmail.com>
2016-04-18 21:27:44 +0100

    Create initial version of version Notebook class

--- Everpobre/app/src/main/java/com/ligartolabs/everpobre/model/Notebook.java
+++ Everpobre/app/src/main/java/com/ligartolabs/everpobre/model/Notebook.java
@@ -4,4 +4,29 @@ package com.ligartolabs.everpobre.model;
  * Created by pmargom on 18/4/16.
  */
 public class Notebook {
+
+    private int id;
+
+    public Notebook(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
 }
